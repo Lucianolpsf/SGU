10ba10854881bf85072732bcd3df1ca2b0404308
karython <karython.unai@gmail.com>
2024-01-23 22:39:42 -0300

    versao atual 23/01

--- SGU/obj/Debug/net8.0/SGU.AssemblyInfo.cs
+++ SGU/obj/Debug/net8.0/SGU.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGU")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d21d9a50757a1ec6dd7f4bbf19927d9c7be72d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGU")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGU")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
