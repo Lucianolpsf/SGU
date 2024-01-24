0f02f069c9ea8e1a8645844b026f1195fc56183a
karython <karython.unai@gmail.com>
2024-01-24 08:42:46 -0300

    area de desenvolvimento

--- SGU/obj/Debug/net8.0/SGU.AssemblyInfo.cs
+++ SGU/obj/Debug/net8.0/SGU.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGU")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d21d9a50757a1ec6dd7f4bbf19927d9c7be72d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ba10854881bf85072732bcd3df1ca2b0404308")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGU")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGU")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
