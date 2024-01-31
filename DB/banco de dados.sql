32131762f54ff82541481e92c50276ca2b1ae022
Luciano Lopes <lucianolpsf@gmail.com>
2024-01-31 17:43:48 -0300

    ajustes pontuais

--- DB/banco de dados.sql
+++ DB/banco de dados.sql
@@ -1,4 +1,5 @@
 create database SGU
+go
 use SGU
 go
 
@@ -6,14 +7,14 @@ CREATE TABLE TipoServico (
     ID int PRIMARY KEY identity (1,1),
     Tipo varchar(50)
 );
-
+go
 CREATE TABLE Servico (
     ID int PRIMARY KEY identity (1,1),
     Tecnica varchar(50),
     Valor float,
     fk_TipoServico_ID int
 );
-
+go
 CREATE TABLE Agendamento (
     ID int PRIMARY KEY identity (1,1),
     dtHoraAgendamento datetime default GETDATE(),
@@ -24,48 +25,46 @@ CREATE TABLE Agendamento (
     fk_Usuario_ID int,
     fk_Servico_ID int
 );
-
+go
 CREATE TABLE Usuario (
     ID int PRIMARY KEY identity (1,1),
     Nome varchar(200) not null,
     Email varchar(200) not null,
     Telefone varchar(50) not null,
 	Senha varchar (50) not null,
-    TipoUsuario BIT default 0
+    TipoUsuario varchar(50)
 );
+go
 
 CREATE TABLE Comentario (
     ID int PRIMARY KEY identity (1,1),
     Descricao text not null,
     fk_Usuario_ID int
 );
+go
  
 ALTER TABLE Servico ADD CONSTRAINT FK_Servico_2
     FOREIGN KEY (fk_TipoServico_ID)
     REFERENCES TipoServico (ID)
     ON DELETE CASCADE;
- 
+go
 ALTER TABLE Agendamento ADD CONSTRAINT FK_Agendamento_2
     FOREIGN KEY (fk_Usuario_ID)
     REFERENCES Usuario (ID)
     ON DELETE CASCADE;
- 
+ go
 ALTER TABLE Agendamento ADD CONSTRAINT FK_Agendamento_3
     FOREIGN KEY (fk_Servico_ID)
     REFERENCES Servico (ID)
     ON DELETE CASCADE;
- 
+ go
 ALTER TABLE Comentario ADD CONSTRAINT FK_Comentario_2
     FOREIGN KEY (fk_Usuario_ID)
     REFERENCES Usuario (ID)
     ON DELETE CASCADE;
-
 go
 insert into Usuario (Nome, Telefone, Email, Senha, TipoUsuario)
 values
 	('karython', '61981035447', 'karython.unai@gmail.com', 'admin', 'administrador')
 go
 
-
-alter table Usuario
-alter column TipoUsuario varchar(50)
