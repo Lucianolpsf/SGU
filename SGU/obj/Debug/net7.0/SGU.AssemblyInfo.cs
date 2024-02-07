759f9ea54bc552a7c81db7033f6fe10b576c0d35
Rodrigojesussilva <106442554+Rodrigojesussilva@users.noreply.github.com>
2024-02-07 20:55:26 -0300

    Resolvendo conf

--- SGU/obj/Debug/net7.0/SGU.AssemblyInfo.cs
+++ SGU/obj/Debug/net7.0/SGU.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGU")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e67eae02d5ee6e68ecfcf03e3dc4fc12e3825ce4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGU")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGU")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
